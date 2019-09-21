c3e903dba6fbd36e391c80e45bca8c93cb6a8b64
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-09-21 14:43:32 +0500

    Update

---
+++ ArraySerializationExample.cs
@@ -0,0 +1,36 @@
+﻿using ToolBox.Serialization;
+using UnityEngine;
+
+public class ArraySerializationExample : MonoBehaviour
+{
+	private void Start()
+	{
+		// Saving data...
+		PlayerData[] playersData = new PlayerData[2];
+
+		playersData[0] = new PlayerData
+		{
+			name = "First Player",
+			age = 20
+		};
+
+		playersData[1] = new PlayerData
+		{
+			name = "Second Player",
+			age = 25
+		};
+
+		GameData.Save(playersData);
+
+		// Loading data...
+		PlayerData[] newPlayersData = new PlayerData[2];
+		newPlayersData = GameData.Load(newPlayersData);
+	}
+}
+
+[System.Serializable]
+public struct PlayerData : ISerializedData
+{
+	public string name;
+	public int age;
+}
