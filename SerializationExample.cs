5ff0db1529b998095d7c4f9c51054d2fc51a9c63
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-09-21 14:38:31 +0500

    Update

---
+++ SerializationExample.cs
@@ -0,0 +1,41 @@
+﻿using ToolBox.Attributes;
+using ToolBox.Serialization;
+using UnityEngine;
+
+public class SerializationExample : MonoBehaviour
+{
+	private void Start()
+	{
+		// Saving data...
+		PlayerData playerData = new PlayerData
+		{
+			name = "My name",
+			age = 20
+		};
+
+		GameData.Save(playerData);
+
+		// Loading data...
+		PlayerData newPlayerData = new PlayerData();
+		newPlayerData = GameData.Load(newPlayerData);
+	}
+
+	[Button("Save")]
+	private void SaveData()
+	{
+		// Saving data...
+		PlayerData playerData = new PlayerData
+		{
+			name = "My name",
+			age = 20
+		};
+
+		GameData.Save(playerData);
+	}
+}
+
+public struct PlayerData : ISerializedData
+{
+	public string name;
+	public int age;
+}
