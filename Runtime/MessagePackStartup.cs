997dca57631626657783a4001a416b582223fcc7
Vladislav Kinyashov <indraayy322@gmail.com>
2025-07-27 23:51:55 +0500

    v3 changes

---
+++ Runtime/MessagePackStartup.cs
@@ -0,0 +1,29 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+using MessagePack.Unity;
+using MessagePack.Unity.Extension;
+using Serializer;
+using ToolBox.Serialization;
+using UnityEngine;
+
+namespace Runtime
+{
+    public static class MessagePackStartup
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Setup()
+        {
+            StaticCompositeResolver.Instance.Register(
+                UnityBlitResolver.Instance,
+                UnityResolver.Instance,
+                StandardResolver.Instance,
+                DataSerializerResolver.Instance
+            );
+
+            var options = ContractlessStandardResolverAllowPrivate.Options.WithResolver(StaticCompositeResolver.Instance);
+
+            DataSerializer.Options = options;
+            MessagePackSerializer.DefaultOptions = options;
+        }
+    }
+}
